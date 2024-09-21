a086e7715d81a0f55d8bc5905d435355a00e4524
Franckyroquai <franckyroquai@gmail.com>
2024-09-21 09:28:12 +0200

    Création plugin administration

---
+++ wp-content/plugins/administration/administration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Administration
+* Plugin URI:  https://www.cookinfamily.fr
+* Description: Ajoutez une page d'administration pour modifier la couleur de fond de votre site WordPress.
+* Version:     1.0.0
+* Author:      OpenClassrooms
+* Author URI:  https://openclassrooms.com
+* Text Domain: administration
+*/
